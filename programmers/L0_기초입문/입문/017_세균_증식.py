9f09690b28ea5d362c6b0db420344fce03e0113e
suim-0811 <suim0811@gmail.com>
2026-02-12 14:26:58 +0900

    [프로그래머스] L0 - 세균 증식 (#17)

---
+++ programmers/L0_기초입문/입문/017_세균_증식.py
@@ -0,0 +1,10 @@
+# 세균 증식
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120910
+# 알고리즘: 기초
+# 작성자: 김수임
+# 작성일: 2026. 02. 12. 14:26:57
+
+def solution(n, t):
+    answer = n * (2 ** t)
+    return answer
